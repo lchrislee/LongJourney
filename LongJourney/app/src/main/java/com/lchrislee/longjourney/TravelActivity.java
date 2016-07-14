f128137c9c96e6d7cb5ab2bccea4814f9538c9d3
Chris Lee <chrisl@usc.edu>
2016-07-14 07:28:47 -0700

    Added simple progressbar for xp. Normally hidden.

--- LongJourney/app/src/main/java/com/lchrislee/longjourney/TravelActivity.java
+++ LongJourney/app/src/main/java/com/lchrislee/longjourney/TravelActivity.java
@@ -2,22 +2,36 @@ package com.lchrislee.longjourney;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.view.BoxInsetLayout;
 import android.support.wearable.view.WatchViewStub;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class TravelActivity extends Activity {
 
-    private TextView mTextView;
+    private BoxInsetLayout box;
+    private ProgressBar xp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+        box = (BoxInsetLayout) findViewById(R.id.travel_layout_box);
+        box.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
             @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                return insets;
+            }
+        });
+        xp = (ProgressBar) findViewById(R.id.travel_progress_xp); // TODO Animate, take a look at http://stackoverflow.com/questions/8035682/animate-progressbar-update-in-android
+        xp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xp.setProgress(xp.getProgress() + 10);
             }
         });
     }
